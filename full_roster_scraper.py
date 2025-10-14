3f7aaabd13aabad10d2acdfa0dc3eb54f170b524
jcj1996-ufc <jakej6745@gmail.com>
2025-10-14 15:43:37 -0500

    Update full_roster_scraper.py

--- full_roster_scraper.py
+++ full_roster_scraper.py
@@ -8,7 +8,7 @@ import requests
 from bs4 import BeautifulSoup
 
 # ---------- Config ----------
-BASE = "https://ufcstats.com"
+BASE = "http://ufcstats.com"
 HEADERS = {
     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/124.0.0.0 Safari/537.36"
 }
